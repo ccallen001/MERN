d8ae2110518eb283dfb6a0b6494572eeda41e355
Chase Allen <ccallen001@gmail.com>
2023-02-13 19:00:52 -0500

    add link back home to 404

--- src/router/routes/route-404/Route404.tsx
+++ src/router/routes/route-404/Route404.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Route404.scss';
 
 export default function Route404() {
@@ -5,6 +6,7 @@ export default function Route404() {
     <div className="Route404">
       <h1>404</h1>
       <h2>Hmm... there's nothing here.</h2>
+      <Link to="/">Let's go Home.</Link>
     </div>
   );
 }
