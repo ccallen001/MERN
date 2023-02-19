dd4f421f34fd983696281d53878d936a77e7c4ef
Chase Allen <ccallen001@gmail.com>
2023-02-19 16:44:53 -0500

    add axios

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,24 +1,31 @@
-import { FETCH_OPTIONS } from '@/types/interface/fetch-options';
+// import { FETCH_OPTIONS } from '@/types/interface/fetch-options';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function useFetch(url: string, options?: FETCH_OPTIONS) {
+export default function useFetch(
+  url: string,
+  /* options?: FETCH_OPTIONS */ body?: Record<string, unknown>
+) {
   const [loading, setLoading] = useState<boolean>(true);
   const [data, setData] = useState<null | Record<string, any>>(null);
 
   useEffect(() => {
     (async () => {
       try {
-        const resp = await fetch(url, options);
-        const respJson = await resp.json();
+        // const resp = await fetch(url, options);
+        // const respJson = await resp.json();
 
-        setData(respJson);
+        // setData(respJson);
+        const { data } = await axios.get(url, body);
+
+        setData(data);
       } catch (err) {
         console.error(err);
       } finally {
         setLoading(false);
       }
     })();
-  }, [url, options]);
+  }, [url, /* options */ body]);
 
   return {
     loading,
